d33542a44c87466be456d036b83d453a8208419b
Matt <aftalion.matt@gmail.com>
2016-08-22 15:11:48 -0700

    changed url

--- server/models/model.js
+++ server/models/model.js
@@ -1,16 +1,10 @@
 var Sequelize = require('sequelize');
 
 //the database "reporting" should already have been created in postgres
-var DATABASE_URL = ('postgres://visicmtsolqsfl:-2UNJ6XVdXlJJLeIHzlkot_RDq@ec2-54-235-102-190.compute-1.amazonaws.com:5432/dfrqqjpqfcc7lp')
+// var DATABASE_URL = ('postgres://visicmtsolqsfl:-2UNJ6XVdXlJJLeIHzlkot_RDq@ec2-54-235-102-190.compute-1.amazonaws.com:5432/dfrqqjpqfcc7lp')
 
 // var sequelize = new Sequelize('postgres://localhost:5432/reporting');
-sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-        ssl: true
-    }
-});
+sequelize = new Sequelize('postgres://visicmtsolqsfl:-2UNJ6XVdXlJJLeIHzlkot_RDq@ec2-54-235-102-190.compute-1.amazonaws.com:5432/dfrqqjpqfcc7lp');
 //test connection
 sequelize
   .authenticate()
